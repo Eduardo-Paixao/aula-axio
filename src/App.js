a899e3a752b164c3a98aefa3da1580306cd64349
Eduardo Paixao <42215395+EPEduardoPaixao@users.noreply.github.com>
2019-02-21 00:15:41 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import axios from 'axios';
+import Cep from './Cep';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Cep/>
+
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/Cep.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import axios from 'axios';
+
+class Cep extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      retorno : '',
+      cep : 60525571
+    };
+  }
+
+  componentDidMount(){
+    this.buscarCEP();
+  }
+
+  componentDidUpdate(){
+    this.buscarCEP();
+  }
+
+
+  buscarCEP(){
+    axios.get('https://viacep.com.br/ws/' + this.state.cep + '/json/')
+      .then(x => this.setState({ retorno : x.data}))
+      .catch(erro => console.log(erro))
+  }
+
+  definirCEP(event){
+    this.setState({ cep : event.target.value});
+  }
+
+  render() {
+    return (
+      <div>
+        <input type="number" value={this.state.cep} onChange={this.definirCEP.bind(this)}></input>
+        <div>
+          Logradouro: {this.state.retorno.logradouro}
+        </div>
+        <div>
+          Cidade: {this.state.retorno.localidade}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Cep;
